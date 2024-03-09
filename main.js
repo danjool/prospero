e1e92c34fe504260ccd3ecb3361a64d6e3bae286
danjool <horrabin@gmail.com>
2024-03-09 11:38:20 -0700

    split out etching shader

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ import * as THREE from 'three';
 import * as dat from 'dat.gui';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { FirstPersonControlsCustom } from '/FirstPersonControlsCustom.js';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
 import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
 import { GTAOPass } from 'three/examples/jsm/postprocessing/GTAOPass';
+
+import { etchingShader } from '/etchingShader.js';
+import { FirstPersonControlsCustom } from '/FirstPersonControlsCustom.js';
 
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -46,186 +48,7 @@ document.body.appendChild(renderer.domElement);
 
 const generatedBlankTexture = new THREE.DataTexture(new Uint8Array([255, 255, 255, 255]), 1, 1, THREE.RGBAFormat); // white texture
 
-let etchingShader = {
-    name: 'EtchingShader',
-    vertexShader: `
-    // uniform vec3 lightDir; // Direction to the light source in camera space
-    uniform float tilingFactor; // To control the tiling of the hatching texture
 
-    varying vec2 vUv;
-    varying float vLightIntensity;
-    varying vec3 vNormalCam;
-    varying vec3 vNormalObj;
-    varying vec3 vNormalWorld;
-    varying vec3 vPositionCamera;
-    varying vec3 vPositionWorld;
-
-    void main() {
-        vec3 lightDir = vec3(0.0, 0.0, 1.0); // Example light direction
-        vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);// Transform vertex position to camera space
-        
-        vUv = uv;
-        vPositionCamera = modelViewPosition.xyz / modelViewPosition.w; // represents the position of the vertex in camera space, z is the depth but in camera space, so it's the distance from the camera
-        vPositionWorld = (modelMatrix * vec4(position, 1.0)).xyz;
-        vNormalCam = normalize(normalMatrix * normal);// Transform normal to camera space
-        vNormalObj = normal;
-        vNormalWorld = normalize((modelMatrix * vec4(normal, 0.0)).xyz);
-        vLightIntensity = max(dot(vNormalCam, normalize(lightDir)), 0.0);// Simple directional light intensity
-
-        gl_Position = projectionMatrix * modelViewPosition;// Transform vertex position to screen space
-    }`,
-    fragmentShader: `
-    uniform sampler2D texture1; 
-    uniform float posCamVsUV;
-    uniform vec3 dirLight1; 
-    uniform vec3 dirLight2; 
-    uniform float tilingFactor; // To control the tiling of the hatching texture
-    uniform float time;
-    uniform float lightFactor;
-    uniform float rampFactor;
-    uniform float gamma;
-    uniform float angleFactor;
-    uniform float theta;
-    uniform float angleClampDivisor;
-    uniform float textureFactor;
-
-    varying vec2 vUv;
-    varying vec3 vPositionCamera;
-    varying float vLightIntensity;
-    varying vec3 vNormalCam;
-    varying vec3 vNormalObj;
-    varying vec3 vPositionWorld;
-
-    float rampFromBlackToWhiteThenBlack(float x) {
-        return 1.0 - 2.0 * abs( mod(x, 1.) - 0.5);
-    }
-
-    float gammaFunction(float x, float gamma) {
-        return pow(x, 1.0/gamma);
-    }
-
-    float atan2(in float y, in float x){
-        float PI = 3.1415926535897932384626433832795;
-        bool s = (abs(x) > abs(y));
-        return mix(PI/2.0 - atan(x,y), atan(y,x), s);
-    }
-
-    vec3 blenderMappingTransform(vec3 inputVector, float rotation, float scale, vec3 translation) {
-        vec3 p = inputVector;
-        float x = p.x * scale;
-        float y = p.y * scale;
-        float z = p.z * scale;
-        float x2 = x * cos(rotation) - y * sin(rotation);
-        float y2 = x * sin(rotation) + y * cos(rotation);
-        return vec3(x2 + translation.x, y2 + translation.y, z + translation.z);
-    }
-
-    float hash(vec2 p) {
-        return fract(sin(dot(p, vec2(127.1, 311.7))) * 43758.5453);
-    }
-    
-    float noise(vec2 p) {
-        vec2 i = floor(p);
-        vec2 f = fract(p);
-        vec2 u = f*f*(3.0-2.0*f);
-    
-        return mix(mix(hash(i + vec2(0.0,0.0)), 
-                       hash(i + vec2(1.0,0.0)), u.x),
-                   mix(hash(i + vec2(0.0,1.0)), 
-                       hash(i + vec2(1.0,1.0)), u.x), u.y);
-    }
-    
-    float perlinNoise(vec2 uv, float scale, int octaves, float persistence) {
-        float total = 0.0;
-        float frequency = scale;
-        float amplitude = 1.0;
-        float maxValue = 0.0;  // Used for normalizing result to 0.0 - 1.0
-        for(int i = 0; i < octaves; i++) {
-            total += noise(uv * frequency) * amplitude;
-            
-            maxValue += amplitude;
-            
-            amplitude *= persistence;
-            frequency *= 2.0;
-        }
-    
-        return total/maxValue;
-    }    
-
-    vec3 fractalPerlinNoise(vec3 p, float scale, int octaves, float persistence, float zFactor) {
-        float n = perlinNoise(p.xy, scale, octaves, persistence);
-        float n2 = perlinNoise(p.xz, scale, octaves, persistence);
-        float n3 = perlinNoise(p.yz, scale, octaves, persistence);
-        return vec3(n, n2, n3);
-    }
-
-    vec3 hash3(vec3 p) {
-        return fract(sin(vec3(dot(p, vec3(127.1, 311.7, 515.3)), dot(p, vec3(269.5, 183.3, 419.3)), dot(p, vec3(419.2, 371.9, 619.3)))) * 43758.5453);
-    }
-
-    void main() {
-        float lighting = max(dot(vNormalObj, normalize(dirLight1)) * .2, 0.0);
-        lighting +=      max(dot(vNormalObj, normalize(dirLight2)) * 0.8, 0.0);
-
-        float angle = atan2(vNormalCam.y, vNormalCam.x); // Angle between the normal and the x-axis
-        float normalizedAngle = (angle + 3.14159) / (2.0 * 3.14159);// Normalize the angle to [0, 1]
-        float steppedAngle = floor(normalizedAngle * 4.) / 4.;// Step the angle to create the lines
-        float rotation = steppedAngle * angleFactor + theta;
-        rotation = 0.;
-        vec3 translating = vec3(0.0, 0.0, 0.0);
-        // posCamVsUV is a uniform that drives the shader to use the position in camera space or the uv coordinates to drive the etching lines
-        // blend between the two by using the posCamVsUV uniform
-        vec3 driver = posCamVsUV * vPositionCamera + (1.0 - posCamVsUV) * vec3(vUv.xy, 0.);
-        // we'll also mutiply tilingFactor by the object's distance from the camera to make the tiling factor smaller as the object gets further away
-        // float tilingFactor = tilingFactor / (vPositionCamera.z);
-        vec3 transformed = blenderMappingTransform(driver, rotation, tilingFactor / (vPositionCamera.z), translating);
-
-        float ramped = rampFromBlackToWhiteThenBlack( transformed.y) ;
-        vec3 rampColor = vec3(ramped, ramped, ramped);
-        rampColor = vec3(gammaFunction(rampColor.x, gamma), gammaFunction(rampColor.y, gamma), gammaFunction(rampColor.z, gamma));
-    
-        vec2 convertedUV = vec2( vUv.x, 1.0 - vUv.y); // because the uv data from the gltf is flipped, we need to convert it by flipping the x and y
-        vec4 textureColor = texture2D(texture1, vUv * 10.0); // * 10.0 to tile the texture
-        float textureIntensity = textureColor.r * 0.3 + textureColor.g * 0.59 + textureColor.b * 0.11;
-
-        // vec3 fpn = fractalPerlinNoise(vPositionCamera, 4.0, 1, 0.5, 0.1); gl_FragColor = vec4(fpn, 1.0);
-        // float n = perlinNoise(vPositionCamera.xy, 10.0, 4, 10.5/vPositionCamera.z);gl_FragColor = vec4(n, n, n, 1.0);
-
-        //gl_FragColor = vec4(hash3(.000001*vPositionCamera.xyz), 1.0);
-    
-        vec3 color = ( lightFactor * lighting ) - rampFactor * rampColor - textureFactor * textureIntensity;
-
-        // uncomment these to visualize the different components of the shader
-        // gl_FragColor = vec4( steppedAngle, steppedAngle, steppedAngle, 1.0);
-        // gl_FragColor = vec4(textureColor.rgb, 1.0);
-        // gl_FragColor = vec4(lighting, lighting, lighting, 1.0);
-        // gl_FragColor = vec4(vNormalObj, 1.0);
-        // gl_FragColor = vec4(vNormalCam.x, vNormalCam.y, vNormalCam.z, 1.0);
-        // gl_FragColor = vec4(rampColor, 1.0);
-        // gl_FragColor = vec4(vPositionCamera.x/vPositionCamera.z, vPositionCamera.y/vPositionCamera.z, 1.0, 1.0);
-        // gl_FragColor = vec4(vUv.x, vUv.y, 1.0, 1.0);
-        // gl_FragColor = vec4(convertedUV.xy, 1.0, 1.0);
-        gl_FragColor = vec4(color.rgb, 1.0);
-    }
-    `,
-
-    uniforms: {
-        time: { value: 0.0 },
-        posCamVsUV: { value: 1.0 },
-        tilingFactor: { value: 100.0},
-        texture1: { value: generatedBlankTexture },
-        dirLight1: { value: new THREE.Vector3(.2, -.2, .8) },
-        dirLight2: { value: new THREE.Vector3(-.4, .2, .1) },
-        lightFactor: { value: 3.0 },
-        textureFactor: { value: 0.5 },
-        rampFactor: { value: 0.9 },
-        gamma: { value: 1.53 },
-        angleFactor: { value: 0.0 },
-        theta: { value: 0.0 },
-        angleClampDivisor: { value: 10.0 },
-
-    }
-};
 
 let gui = new dat.GUI();
 gui.add(etchingShader.uniforms.tilingFactor, 'value', 0, 300).name('Tiling Factor');
